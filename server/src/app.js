c9f7e2192bcb8d28ab5a7d4fd5e173ff2fc8623c
Alisaada1 <132406758+Alisaada1@users.noreply.github.com>
2026-08-20 02:36:21 +0300

    Set trust proxy setting for Express app

--- server/src/app.js
+++ server/src/app.js
@@ -31,6 +31,7 @@ const distDir = path.resolve(__dirname, "../../client/dist");
 const indexHtml = path.join(distDir, "index.html");
 
 const app = express();
+app.set("trust proxy", 1);
 
 app.use(
   helmet({
